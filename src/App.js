ad7982790535a9383fcccfc07db8658aff54d670
ekeneakubue <ekeneakubue@gmail.com>
2024-05-07 12:28:33 +0100

    modified Package.json and added Category Folder and Sidenavbar folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import SideNavbar from './components/sideNavbar/SideNavbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <SideNavbar/>
     </div>
   );
 }
