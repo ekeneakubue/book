a1c8dde7323097a9578dfe1dd56121be4ab151bc
ekeneakubue <ekeneakubue@gmail.com>
2024-05-09 23:56:26 +0100

    modified SideNavbar, TopNavbar and Integrate Dashboard

--- src/components/topNavbar/TopNavbar.jsx
+++ src/components/topNavbar/TopNavbar.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import './TopNavbar.css'
+import { CiSearch } from "react-icons/ci";
+import { MdMic, MdOutlineNotifications } from "react-icons/md";
+import { PiBookOpenText } from "react-icons/pi";
+import { RxAvatar } from "react-icons/rx";
 
 export default function TopNavbar() {
   return (
-    <div>TopNavbar</div>
+    <div className="top-navbar">
+      <div className="left-bar">
+        <button>Basic </button>
+        <form>
+          <div className="search-box">
+            <CiSearch className='icon'/>
+            <input type="text" placeholder='Search in basic form'/>
+            <MdMic className='icon'/>
+          </div>
+        </form> 
+      </div>
+      <div className="right-bar">
+        <PiBookOpenText />
+        <MdOutlineNotifications />
+        <RxAvatar />
+      </div>
+    </div>
   )
 }
+
